7dba9156ad5198954d1ea368519b7acbfe55afbd
Arthur Vinicius <arthur.vinicius@portoseguro.com.br>
2023-03-09 12:08:20 -0300

    2. Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import ImgIgniteLogo from "../assets/ignite-logo.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={ImgIgniteLogo} alt="Logo Ignite Feed" />
     </header>
   );
 }
