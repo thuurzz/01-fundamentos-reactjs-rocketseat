86b35feb29d7fc1805de1efacec82165edab93a5
Arthur Vinicius <arthur.vinicius@portoseguro.com.br>
2023-03-09 11:10:29 -0300

    1. Componentes e Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>Hello World!</h1>;
-}
+import { Post } from "./Post";
 
-export default App;
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Arthur Silva"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sapiente voluptatum similique tempore voluptas veritatis ea a at iure non quasi minus maxime impedit, quia rerum nobis praesentium, ipsum reprehenderit!"
+      />
+      <Post
+        author="Larrisa Ionafa"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sapiente voluptatum similique tempore voluptas veritatis ea a at iure non quasi minus maxime impedit, quia rerum nobis praesentium, ipsum reprehenderit!"
+      />
+    </div>
+  );
+}
