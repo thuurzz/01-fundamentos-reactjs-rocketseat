41b066c87a51a40aab5ce6a0ea0c148a981471ec
Arthur Vinicius <arthur.vinicius@portoseguro.com.br>
2023-03-11 09:39:04 -0300

    2. wip: Componente Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,18 @@ import styles from "./Sidebar.module.css";
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img 
-      className={styles.cover}
-      src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
 
       <div className={styles.profile}>
-        <strong>Nome</strong>
-        <span>Cargo</span>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/53263896?v=4"
+        />
+        <strong>Arthur Vinicius</strong>
+        <span>Arq. Tecnologia Jr</span>
       </div>
 
       <footer>
